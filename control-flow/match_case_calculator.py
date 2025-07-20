6c8f9d7935b1d58fb5eaef4852faade8f3aafa21
Ednah Bridget <bridgetkakah@gmail.com>
2025-07-19 18:51:54 -0700

    Fix: Add match statement and result output message

--- control-flow/match_case_calculator.py
+++ control-flow/match_case_calculator.py
@@ -1,22 +1,21 @@
 num1 = float(input("Enter the first number: "))
 num2 = float(input("Enter the second number: "))
-
 operation = input("Choose the operation (+, -, *, /): ")
 
 if operation == "+":
     result = num1 + num2
-    print(f"The result is {result}.")
 elif operation == "-":
     result = num1 - num2
-    print(f"The result is {result}.")
 elif operation == "*":
     result = num1 * num2
-    print(f"The result is {result}.")
 elif operation == "/":
     if num2 != 0:
         result = num1 / num2
-        print(f"The result is {result}.")
     else:
         print("Cannot divide by zero.")
+        exit()
 else:
-    print("Invalid operation selected.")
+    print("Invalid operation.")
+    exit()
+
+print("The result is", result)
