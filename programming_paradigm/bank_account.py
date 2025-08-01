ad5ed723d58e0f0c930621b028fb37d2b2e04944
Ednah Bridget <bidgetkakah@gmail.com>
2025-08-02 00:19:57 +0300

    Add Simple BankAccount class and CLI interface

---
+++ programming_paradigm/bank_account.py
@@ -0,0 +1,17 @@
+class BankAccount:
+    def __init__(self, account_balance=0):
+        """Initialize the bank account with an optional initial balance."""
+        self.account_balance = account_balance
+
+    def deposit(self, amount):
+        if amount > 0:
+            self.account_balance += amount
+
+    def withdraw(self, amount):
+        if 0 < amount <= self.account_balance:
+            self.account_balance -= amount
+            return True
+        return False
+
+    def display_balance(self):
+        print(f"Current Balance: ${self.account_balance:}")
